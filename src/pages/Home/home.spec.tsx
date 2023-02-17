d299d938a5e64c422d8151bd5455be99f744244b
Jr Bytes <itamazor@gmail.com>
2023-02-17 11:06:42 -0300

    updated home page, tested styled-component

--- src/pages/Home/home.spec.tsx
+++ src/pages/Home/home.spec.tsx
@@ -2,12 +2,16 @@ import { screen } from '@testing-library/react'
 
 import { renderWithTheme } from '../../utils/helpers'
 import { Home } from '.'
+import theme from '../../styles/theme'
 
 describe('<Home />', () => {
   it('should be able to render the home page', () => {
-    renderWithTheme(<Home />)
+    const { container } = renderWithTheme(<Home />)
 
     expect(screen.getByRole('heading', { name: /home/i })).toBeInTheDocument()
     expect(screen.getByText(/hello world/i)).toBeInTheDocument()
+    expect(container.firstChild).toHaveStyle({
+      maxWidth: theme.grid.container,
+    })
   })
 })
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,6 +2,7 @@ import styled, { css } from 'styled-components'
 
 export const Wrapper = styled.div`
   ${({ theme }) => css`
+    width: 100vw;
     max-width: ${theme.grid.container};
     margin: 0 auto;
   `}
