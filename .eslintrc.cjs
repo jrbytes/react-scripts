50b932485ff32b11392f105ee2fda2f2d688623f
Jr Bytes <itamazor@gmail.com>
2023-05-12 20:23:10 -0300

    add typescript eslint - no unused expressions to eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -27,5 +27,6 @@ module.exports = {
     'react/jsx-no-constructed-context-values': 0,
     'import/no-extraneous-dependencies': 0,
     'react/jsx-props-no-spreading': 0,
+    '@typescript-eslint/no-unused-expressions': 0,
   },
 }
