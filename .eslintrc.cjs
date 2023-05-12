aef6393a909b69a6de592db6113b78a7c8b0f552
Jr Bytes <itamazor@gmail.com>
2023-05-12 19:43:52 -0300

    add jsx props no spreading to rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -26,5 +26,6 @@ module.exports = {
     '@typescript-eslint/no-shadow': 0,
     'react/jsx-no-constructed-context-values': 0,
     'import/no-extraneous-dependencies': 0,
+    'react/jsx-props-no-spreading': 0,
   },
 }
